6fe4d24dd524621601a17d49e4ae99382fd5b050
HunterXVD11 <81260156+HunterXVD11@users.noreply.github.com>
2021-12-14 17:56:04 -0300

    Criando arquivos pra rodar o programa

---
+++ cmake-build-debug/CMakeFiles/Grafo_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Grafo_2.dir/grafo.c.obj"
+  "Grafo_2.exe"
+  "Grafo_2.exe.manifest"
+  "Grafo_2.pdb"
+  "libGrafo_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Grafo_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
